f0e546b6cd13621a51fb49cf0acc0b136e646655
LieraBaran <babamota@ukr.net>
2023-03-28 10:07:51 +0300

    count_sentences

--- file.py
+++ file.py
@@ -9,3 +9,30 @@ def count_words(filename):
         words = content.split()
         word_count = len(words)
         return word_count
+    
+def count_sentences(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+        # Видаляємо зайві пробіли з початку та кінця тексту
+        content = content.strip()
+        # Рахуємо кількість речень
+        sentence_count = 0
+        for char in content:
+            if char in [".", "!", "?"]:
+                sentence_count += 1
+        return sentence_count
+    
+def count_words_and_sentences(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+        # Видаляємо зайві пробіли з початку та кінця тексту
+        content = content.strip()
+        # Рахуємо кількість слів
+        words = content.split()
+        word_count = len(words)
+        # Рахуємо кількість речень
+        sentence_count = 0
+        for char in content:
+            if char in [".", "!", "?"]:
+                sentence_count += 1
+        return "Кількість слів " , word_count, "Кількість речень ",sentence_count
