5cac2c87fa24f61dfe2f1acaca798332963b800d
LieraBaran <babamota@ukr.net>
2023-03-28 12:22:47 +0300

    test_2

--- test/test_count_words_and_sentences.py
+++ test/test_count_words_and_sentences.py
@@ -1,6 +1,20 @@
 import pytest
-from file import count_words_and_sentences
+import os
+from file import count_words,count_sentences
 
-@pytest.mark.parametrize("expected_result, filename", [(10, "test.txt"), (15, "test2.txt")])
-def test_count_words_and_sentences(expected_result, filename):
-    assert count_words_and_sentences(filename) == expected_result
+
+@pytest.fixture
+def text_file():
+    content = "Hello, world! This is a test.\nThis is only a test.\n"
+    filename = "test.txt"
+    with open(filename, "w") as f:
+        f.write(content)
+    yield filename
+    os.remove(filename)
+
+def test_count_words(text_file):
+    assert count_words(text_file) == 11
+
+@pytest.mark.parametrize("expected_count", [3, 3])
+def test_count_sentences(text_file, expected_count):
+    assert count_sentences(text_file) == expected_count
